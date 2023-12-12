141e4b141eb3085602a550f370c84e1c9af246af
Konrad Kudlacz <konrad.kudlacz@gmail.com>
2023-12-12 20:09:38 +0100

    blink led in while loop

--- door_phone/main.cpp
+++ door_phone/main.cpp
@@ -20,6 +20,15 @@ uint32_t GetMsSinceReboot() {
   const auto absolute = get_absolute_time();
   return to_ms_since_boot(absolute);
 }
+void ToggleCywLed(const uint32_t time_since_boot_ms) {
+  static constexpr uint32_t toggle_period_ms = 500;
+  static uint32_t next_toggle_timestamp_ms = 0;
+  if (time_since_boot_ms > next_toggle_timestamp_ms) {
+    const bool pin_val = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !pin_val);
+    next_toggle_timestamp_ms = time_since_boot_ms + toggle_period_ms;
+  }
+}
 }  // namespace
 
 CaSignalProcessor* ca_signal_processor_ptr = nullptr;
@@ -31,19 +40,13 @@ bool PeriodicCallbackCaSignalProcessor(struct repeating_timer* t) {
   return true;
 }
 
-bool PeriodicCallbackToggleLed(struct repeating_timer* t) {
-  (void)t;
-  const bool pin_val = cyw43_arch_gpio_get(CYW43_WL_GPIO_LED_PIN);
-  cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, !pin_val);
-  return true;
-}
 int main() {
   stdio_init_all();
   if (cyw43_arch_init()) {
     printf("WiFi init failed");
     return -1;
   }
-  InitOutputPins();
+  ::InitOutputPins();
   const auto adc_ca_channel = RP2040Adc::EChannelId::CHANNEL_GPIO_28;
   RP2040Adc adc({adc_ca_channel});
   CaSignalProcessor ca_signal_processor(
@@ -53,13 +56,8 @@ int main() {
   const int32_t ca_signal_sample_rate_ms = 5;
   add_repeating_timer_ms(-ca_signal_sample_rate_ms,
                          PeriodicCallbackCaSignalProcessor, NULL, &timer_ca);
-  //   struct repeating_timer timer_led;
-  //   const int32_t toggle_led_rate_ms = 500;
-  //   add_repeating_timer_ms(-toggle_led_rate_ms, PeriodicCallbackToggleLed,
-  //   NULL,
-  //                          &timer_led);
-  //   printf("repeating timer led added \n");
   while (true) {
-    ;
+    const uint32_t time_since_boot_ms = ::GetMsSinceReboot();
+    ::ToggleCywLed(time_since_boot_ms);
   }
 }
